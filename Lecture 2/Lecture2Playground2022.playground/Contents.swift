f908a3c3b94ccf07eddf991a48a126156e568d4b
Romas Petkus <romas.voltasit@gmail.com>
2022-09-21 22:12:02 +0300

    paskaitos juodrastis

---
+++ Lecture 2/Lecture2Playground2022.playground/Contents.swift
@@ -0,0 +1,105 @@
+//var manoKintamasis: Int = 7
+//let maxLoginAttemps: Int = 5
+//
+//manoKintamasis = 4
+//manoKintamasis = maxLoginAttemps
+//
+//print(manoKintamasis * 4)
+//
+////manoKintamasis = maxLoginAttemps + 5
+//
+////print(manoKintamasis - 4)
+//print(manoKintamasis + 4)
+//
+////print(manoKintamasis / maxLoginAttemps)
+//
+//
+//// kvieciu say hello funkcija fesiokerfkserfioesrf
+///* */
+//
+//sayHello("200")
+//
+///// Sita funckija isprinta parama
+///// - Parameter param: parametras kuri isspaudina i konsole
+///// - Returns: vieneta
+//func sayHello(_ param: String) -> Int {
+//    print(param)
+//    return 1
+//}
+//
+
+//var skaiciuRinkinys: [Int] = [1, 5, 8]
+//
+////print(skaiciuRinkinys.count)
+////
+//
+//skaiciuRinkinys.append(36)
+//
+//var a = 4
+
+//var manoVaikuAmzius: [String : Int] = [String: Int]()
+//
+//var darbuotojoPareigosPagalKoda: [Int: String] = [105: "Apsauginis"]
+//
+//
+//darbuotojoPareigosPagalKoda[105] = "Apsauginis"
+//darbuotojoPareigosPagalKoda[1003] = "Progamuotojas"
+//
+//print(darbuotojoPareigosPagalKoda[105])
+
+//func pasisveikinti() {
+//    print("sveiki")
+//}
+//
+//
+//pasisveikinti()
+//pasisveikinti()
+//pasisveikinti()
+//
+//func pasisveikintiAsmeniskai(_ vardas: String, _ vardas2: String, pavarde: String) {
+//    print("Sveikas \(vardas), \(vardas2) - \(pavarde)")
+//}
+//var manoVardas = "Mantas"
+//
+//func suma(a: Int, b: Int) -> String
+//{
+//    return "Mano gauta suma: \(a + b)"
+//}
+//
+//pasisveikintiAsmeniskai(manoVardas, "Petras", pavarde: "Petraitis")
+//
+//let skaiciuSuma: String = suma(a: 5, b: 11)
+//
+//print(skaiciuSuma)
+
+//let manoAmzius = "6"
+//
+//if manoAmzius == "Jonas" {
+//    print("Tu vyresnis nei 10 metu")
+//}
+
+//if manoAmzius < 7 {
+//    print("Tau maziau nei 7 metai")
+//} else {
+//    print("Tau yra maziau arba desimt metu")
+//}
+
+//if 30 % 5 == 0 {
+//    print("25 dalinasi is 5")
+//}
+//else {
+//    print("nesidalina")
+//}
+
+// Namu Darbas: Skaidrese assigment 3
+// func1(a, b) -> Bool
+// func2(a, b) ----------- kviecia func1 ir tikrina ar dalinasi a ir b ir atprintina atitinkama zinute
+// kad dokumentuot funkcija spaudziam option + command + /
+var arTiesa: Bool = false
+
+if arTiesa {
+    print("Taip tikrai tiesa ")
+}
+else {
+    print("Netiesa")
+}
